9349b903895096b6213e6d9bbc1c55abeaaf8d81
lakshmi <krg.lakshminarasimhan@gmail.com>
2017-09-19 13:32:22 -0400

    include aug flag for selective augmentation

--- data_loader.py
+++ data_loader.py
@@ -40,7 +40,7 @@ def modifySample(label,image,com3D,M,sigma_sc=0.05,sigma_com=5.):
 
     return label,image,com3D,M
 
-def read_and_decode(filename_queue,target_size,label_shape):
+def read_and_decode(filename_queue,target_size,label_shape,data_augment):
     reader = tf.TFRecordReader()
     _,serialized_example = reader.read(filename_queue)
     features = tf.parse_single_example(serialized_example,features = {
@@ -65,15 +65,16 @@ def read_and_decode(filename_queue,target_size,label_shape):
     com3D = tf.reshape(com3D,(3,))
     M=tf.reshape(M,(3,3))
 
-    '''
-    Augmentations happen here! Take each sample and pick an aug mode 
-    and modify the image and the respective label
-    '''
-    label,image,com3D,M = modifySample(label,image,com3D,M)
-    label = tf.reshape(label,[label_shape,])
-    '''
-    Ends here!
-    '''
+    if (data_augment):
+        '''
+        Augmentations happen here! Take each sample and pick an aug mode 
+        and modify the image and the respective label
+        '''
+        label,image,com3D,M = modifySample(label,image,com3D,M)
+        label = tf.reshape(label,[label_shape,])
+        '''
+        Ends here!
+        '''
 
     #print("com3D shape:{}".format(com3D.get_shape().as_list()))
     #print("M shape:{}".format(M.get_shape().as_list()))
@@ -87,14 +88,14 @@ def read_and_decode(filename_queue,target_size,label_shape):
     #return label,image
 
 
-def inputs(tfrecord_file,num_epochs,image_target_size,label_shape,batch_size):
+def inputs(tfrecord_file,num_epochs,image_target_size,label_shape,batch_size,data_augment=False):
     print('in input!')
     with tf.name_scope('input'):
         if os.path.exists(tfrecord_file) is False:
             print("{} not exists".format(tfrecord_file))
         # returns a queue. adds a queue runner for the queue to the current graph's QUEUE_RUNNER
         filename_queue = tf.train.string_input_producer([tfrecord_file],num_epochs=num_epochs)
-        label, image, com3D, M = read_and_decode(filename_queue = filename_queue,target_size=image_target_size,label_shape = label_shape)
+        label, image, com3D, M = read_and_decode(filename_queue = filename_queue,target_size=image_target_size,label_shape = label_shape,data_augment = data_augment)
         # return a list or dictionary. adds 1) a shuffling queue into which tensors are enqueued; 2) a dequeue_many operation to create batches
         # from the queue 3) a queue runner to QUEUE_RUNNER collection , to enqueue the tensors.
         data,labels,com3Ds,Ms = tf.train.shuffle_batch([image,label,com3D,M],batch_size=batch_size,num_threads=2,capacity=100+3*batch_size,min_after_dequeue=1)
