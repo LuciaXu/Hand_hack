fixing online rotation augmentation

Datareader.py
@@ -34,9 +34,9 @@ def Datareader(config):
     cwd = os.getcwd()
     print(cwd)
     rng = np.random.RandomState(23455)
-    ds = NYUImporter('/media/data_cifs/lu/NYU/dataset',useCache=True)
-    Seq1= ds.loadAugSequence('train', shuffle=True, rng=rng, docom=True,allJoints=True,num_aug=32)
-    #Seq1 = ds.loadSequence('train', shuffle=True, rng=rng, docom=True, allJoints=True, Nmax=1)
+    ds = NYUImporter('/media/data_cifs/lu/NYU/dataset',useCache=False)
+    #Seq1= ds.loadAugSequence('train', shuffle=True, rng=rng, docom=True,allJoints=True,num_aug=1,Nmax=1)
+    Seq1 = ds.loadSequence('train', shuffle=True, rng=rng, docom=True, allJoints=True, Nmax=1)
     trainSeqs = [Seq1]
 
     trainDataSet = NYUDataset(imgSeqs = trainSeqs)
@@ -68,12 +68,11 @@ def Datareader(config):
     print('Get {} training data.'.format(train_data.shape[0]))
     create_tf_record(train_data,train_gt3D,os.path.join(config.tfrecord_dir,config.train_tfrecords),config,train_com3D,train_M)
     print('create testing data and validation data')
-    Seq2 = ds.loadSequence('test', shuffle=True, rng=rng,docom=True,allJoints=True)
+    Seq2 = ds.loadSequence('test', shuffle=True, rng=rng,docom=True,allJoints=True,Nmax=4)
     testSeqs = [Seq2]
     testDataSet = NYUDataset(imgSeqs=testSeqs,val_prop=0.3)
     test_data, test_gt3D,val_data,val_gt3D,testconfig,test_com3D,val_com3D,test_M,val_M = testDataSet.imgStackDepthOnly('test')
-    create_tf_record(val_data, val_gt3D, os.path.join(config.tfrecord_dir, config.val_tfrecords), config, val_com3D,
-                     val_M)
+    create_tf_record(val_data, val_gt3D, os.path.join(config.tfrecord_dir, config.val_tfrecords), config, val_com3D,val_M)
     create_tf_record(test_data, test_gt3D, os.path.join(config.tfrecord_dir, config.test_tfrecords), config,test_com3D,test_M)
     print('Get {} test data'.format(test_data.shape[0]))
     print('Get {} validation data'.format(val_data.shape[0]))

Importer.py
@@ -376,27 +376,13 @@ class NYUImporter(Importer):
                 gt3Dorig[jt,0] = joints3D[line,jj,0]
                 gt3Dorig[jt,1] = joints3D[line,jj,1]
                 gt3Dorig[jt,2] = joints3D[line,jj,2]
-
+                jt=jt+1
                 # #check if joint2D can be translated to joint3d by jointsImgto3D
                 # print gtorig[jj]
                 # gttrans = self.jointImgTo3D(gtorig[jj])
                 # print(gttrans)
                 # print(gt3Dorig[jj])
 
-                jt +=1
-                # # joints3D can be translated to joints2D by joint3DToImg
-                # if jj is 0:
-                #     print gt3Dorig[jj]
-                #     gttrans = self.joint3DToImg(gt3Dorig[jj])
-                #     print gttrans
-                #     print gtorig[jj]
-
-
-
-
-
-            #print gt3Dorig
-            #showAnnotatedDepth(ICVLFrame(dpt, gtorig, gtorig, 0, gt3Dorig, gt3Dorig, 0, dptFileName, ''))
 
             # Detect hand
             hd = HandDetector(dpt, self.fx, self.fy, importer = self)
@@ -420,6 +406,7 @@ class NYUImporter(Importer):
             #print("gt3Dorig 34 is{}".format(gt3Dorig[34]))
             #print("com3D is {}".format(com3D))
 
+            #print(gt3Dorig)
             gt3Dcrop = gt3Dorig - com3D #normalize to com
 
             gtcrop = np.zeros((gtorig.shape[0],3),np.float32)
@@ -440,7 +427,7 @@ class NYUImporter(Importer):
             #gtcrop_test = trans3DsToImg(gt3Dcrop,com3D,M)
             #showImageLable(dpt,gtcrop_test)
 
-            print(gt3Dcrop,com3D,M)
+            #print(gt3Dcrop,com3D,M)
             data.append(ICVLFrame(dpt.astype(np.float32),gtorig,gtcrop,M,gt3Dorig,gt3Dcrop,com3D,dptFileName,'') )
             pbar.update(i)
             i+=1

train_cnn_networks.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import tensorflow as tf
 #from data.data_loader import inputs
 from data_loader import inputs
-from check_fun import showdepth, showImagefromArray,showImageLable,trans3DsToImg,showImageLableCom,showImageJoints,showImageJointsandResults
+from check_fun import showdepth, showImagefromArray,showImageLable,trans3DsToImg,showImageLableCom,showImageJoints,showImageJointsandResults, showJointsOnly
 #from tf_fun import regression_mse, correlation, make_dir, \
 #    fine_tune_prepare_layers, ft_optimizer_list
 from pose_evaluation import getMeanError,getMeanError_np,getMean_np,getMeanError_train
@@ -46,7 +46,7 @@ def train_model(config,seqconfig):
                                                             image_target_size=config.image_target_size,
                                                             label_shape=config.num_classes,
                                                             batch_size=config.val_batch)
-    with tf.device('/gpu:2'):
+    with tf.device('/gpu:0'):
         with tf.variable_scope("cnn") as scope:
             print("create training graph:")
             model=cnn_model_struct()
@@ -94,6 +94,26 @@ def train_model(config,seqconfig):
         threads = tf.train.start_queue_runners(coord=coord)
         try:
             while not coord.should_stop():
+
+                gt = train_labels.eval()
+                gt_0 = gt[0]
+                #print(gt_0)
+                imgs = train_images.eval()
+                img_0 = imgs[0]
+                #print(img_0)
+                coms = com3Ds.eval()
+                coms_0 = coms[0]
+                ms = Ms.eval()
+                ms_0 = ms[0]
+                im = img_0.reshape([128, 128])
+                gt0 = gt_0.reshape(36,3)
+                gt0 = gt0 * 150
+                #print (gt0,coms_0,ms_0)
+                jcrop = trans3DsToImg(gt0, coms_0, ms_0)
+
+                showJointsOnly(im,jcrop)
+                print(gt.shape)
+
                 _,image_np,image_label,image_coms,image_Ms,tr_error,tr_loss,tr_loss_wd = sess.run([train_op,train_images,train_labels,com3Ds,Ms,train_error,loss,loss_wd])
                 print("step={},loss={},losswd={},error={} mm".format(step,tr_loss,tr_loss_wd,tr_error))
 